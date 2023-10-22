d1525df282cd3bb02e57de0e4d20d6c80a791455
Pacharapol Withayasakpunt <polv@polv.cc>
2023-10-22 13:38:49 +0700

    remove assets

--- src/dict.ts
+++ src/dict.ts
@@ -26,8 +26,9 @@ export class Dict extends Dexie {
     if ((await this.voc.count()) === 0) {
       await this.voc.bulkPut(
         (
-          (await import('../assets/jmdict-eng-common-3.5.0.json'))
-            .default as JMdict
+          (await fetch('/jmdict-eng-common-3.5.0.json').then((r) =>
+            r.json(),
+          )) as JMdict
         ).words,
       );
     }
@@ -35,8 +36,9 @@ export class Dict extends Dexie {
     if ((await this.kan.count()) === 0) {
       await this.kan.bulkPut(
         (
-          (await import('../assets/kanjidic2-en-3.5.0.json'))
-            .default as Kanjidic2
+          (await fetch('/kanjidic2-en-3.5.0.json').then((r) =>
+            r.json(),
+          )) as Kanjidic2
         ).characters,
       );
     }
